c7de767b2003a813c6484efa5fd5c164a8248c23
siddiquenaim <naim.niter.tex06@gmail.com>
2023-05-26 14:45:15 +0600

    testing git repo

--- src/pages/home/Banner/Banner.jsx
+++ src/pages/home/Banner/Banner.jsx
@@ -9,7 +9,9 @@ const Banner = () => {
     <div className="hero min-h-screen bg-banner mb-24">
       <div className="hero-content lg:text-center text-neutral-content">
         <div className="w-[90%] lg:w-[75%]">
-          <h1 className="mb-5 text-5xl font-bold">Discover Turkish Cuisine</h1>
+          <h1 className="mb-5 text-5xl font-bold">
+            Discover Turkish Cuisine O
+          </h1>
           <p className="mb-5 lg:w-[75%] lg:mx-auto text-xl">
             Explore the rich flavors of Turkish cuisine with TasteTurkish. From
             mouthwatering kebabs to delectable baklava, our recipes showcase the
